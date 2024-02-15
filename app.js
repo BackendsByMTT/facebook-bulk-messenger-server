1ede05094d9956c830a47c4ce49903848294d496
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-02-15 10:20:37 +0530

    socket

--- app.js
+++ app.js
@@ -1,44 +1,63 @@
+"use client";
+
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const io = require("socket.io");
 const router = require("./routes/userRoutes");
-//CORS
+const server = http.createServer(app);
+const io = socketIo(server);
+// CORS
 const corsOrigin = {
   origin: "*",
 };
 app.use(cors(corsOrigin));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set("socketio", io);
-//PORT & DB_URL
+
+// PORT & DB_URL
 const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL;
 
-//CONNECTING db-
-main().then(() => console.log("databse connected"));
-main().catch((err) => console.log(err, "err"));
+// CONNECTING db
+main()
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err, "error"));
 
 async function main() {
   await mongoose.connect(DB_URL);
 }
 
-//ROUTES
+// ROUTES
 app.use("/", router);
 
-//SOCKET IO
+// SOCKET IO
+let ids = [];
+let currentIndex = 0;
+
 io.on("connection", (socket) => {
-  socket.on("user-data", (userData) => {
-    console.log(userData);
+  socket.on("user-data", (message, id, time) => {
+    console.log(message, id, time);
+    ids.push(id);
+    if (ids.length >= 1 || currentIndex >= ids.length) {
+      const batch = ids.slice(0, 2);
+      console.log(batch);
+      // currentIndex += 3;
+
+      socket.emit("sendData", message, batch);
+
+      if (currentIndex >= ids.length) {
+        currentIndex = 0;
+        ids = [];
+      }
+    }
   });
 });
 
-
-
-
-//LISTING TO SERVER
-app.listen(PORT, () => {
+// LISTENING TO SERVER
+server.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
 });
